d7db8c02534b6b5efa371ca7a6c03c58bd7a38fb
Ethan Arrowood <ethan.arrowood@gmail.com>
2017-03-13 10:15:22 -0400

    Matrix class created. Inverse class inherits matrix.

--- src/finalProject/Main.java
+++ src/finalProject/Main.java
@@ -4,11 +4,39 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double[][] matrixA = {{1, 2}, {3, 4}};
+//		double[][] mA = {{1, 2}, {3, 4}};
+//		
+//		Matrix matrixA = new Matrix(mA);
+//		matrixA.solveInverse();
+//		System.out.println(matrixA.inverse.toString());
+//		
+//		Matrix m = new Matrix(mA);
+//		System.out.println(m.toString());
+//		double[][] matrixB = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+//		m.setMatrix(matrixB);
+//		System.out.println(m.toString());
+//		double[][] matrixC = {{1, 2}, {3, 4}, {5, 6} , {7, 8}};
+//		m.setMatrix(matrixC);
+//		System.out.println(m.toString());
 		
-		Inverse inverseA = new Inverse(matrixA);
 		
-		System.out.println(inverseA.toString());
+		
+		double[][] b = {{10, 20}, {30, 40}}; //create multidim array
+		
+		Matrix matrixB = new Matrix(b); //create instance of Matrix
+		
+		matrixB.solveInverse(); //get the inverse of matrixB
+		
+		System.out.println(matrixB.inverse.toString()); //print to console
+		
+		double[][] b2 = {{1, 2}, {3, 4}}; //create new multidim array
+		
+		matrixB.setMatrix(b2); //set the matrix 
+		
+		//By resetting the multidim array in matrixB we 
+		// are auto generating the new inverse (if it exists)
+		
+		System.out.println(matrixB.inverse.toString()); //print new inverse to console
 	}
 
 }
