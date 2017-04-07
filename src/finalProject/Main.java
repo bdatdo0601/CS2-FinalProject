f34d7af1ce0ed4b4e6a24122442cab777391188e
Ethan Arrowood <ethan.arrowood@gmail.com>
2017-04-06 20:50:57 -0400

    Commons Math added and working on parser

--- src/finalProject/Main.java
+++ src/finalProject/Main.java
@@ -1,15 +1,13 @@
 package finalProject;
 
+import java.util.ArrayList;
 import java.util.Scanner;
-
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.DecompositionSolver;
-import org.apache.commons.math3.linear.EigenDecomposition;
-import org.apache.commons.math3.linear.RealMatrix;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		
 //		System.out.println("Add Matrices Example");
@@ -31,9 +29,46 @@ public class Main {
 //		Matrix inverseC = Matrix.getInverse(C);
 //		System.out.println(inverseC.toString());
 
-		double[][] data = { {1, 0}, {0, 1} };
-		RealMatrix n = new Array2DRowRealMatrix(data);
-		EigenDecomposition eigs = new EigenDecomposition(n);
+//		double[][] data = { {1, 0}, {0, 1} };
+//		RealMatrix n = new Array2DRowRealMatrix(data);
+//		EigenDecomposition eigs = new EigenDecomposition(n);
+//		double[] eigVals = eigs.getRealEigenvalues();
+		
+//		System.out.println("Row Reduce Example");
+//		Matrix A = new Matrix(2, 2);
+//		System.out.println("Matrix A: ");
+//		System.out.println(A.toString());
+//		System.out.println("Row reduction steps:");
+//		Matrix B = Matrix.rowReduce(A);
+//		System.out.println("Matrix B: ");
+//		System.out.println(B.toString());
+		
+		
+		// Example input string = "[1 2; 3 4]"
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your array in Matlab syntax:");
+		String in = input.nextLine();
+		double[][] matrix = new double[2][2];
+		
+		String validTokenPattern = "[\\[\\]\\d\\;\\s]+";
+		Pattern validTokens = Pattern.compile(validTokenPattern);
+	
+		if ( !Pattern.matches(validTokenPattern, in) ) {
+			throw new Exception("String does not match.");
+		} // else continue
+		
+		String numberPattern = "\\d+\\.*\\d*\\s*[\\,|\\;]*";
+		Pattern numberMatcher = Pattern.compile(numberPattern);
+		
+		// get all the matches
+		Matcher m = numberMatcher.matcher(in);
+		
+		ArrayList<Double> outer = new ArrayList<>();
+		
+		while(m.find()) {
+			System.out.println(m);
+		}
+		
 		
 	}
 }
--- src/finalProject/Matrix.java
+++ src/finalProject/Matrix.java
@@ -162,7 +162,17 @@ public class Matrix {
 			double d = matrix[1][1];
 			boolean mulDifTest = (a*d) - (b*c) != 0;
 			return is2by2 && mulDifTest;
-		} else { 
+		} else if(rowCount == colCount) { 
+			Matrix reduced = rowReduce(new Matrix(matrix));
+			double[][] rm = reduced.getMatrix();
+			boolean isInv = true;
+			for ( int i = 0; i < rm.length; i++) {
+				for ( int j = 0; j < rm[i].length; j++) {
+					if( j == i && rm[i][j] == 0 ) isInv = false;
+				}
+			}
+			return isInv;
+		} else {
 			return false;
 		}
 	}
@@ -189,7 +199,7 @@ public class Matrix {
 	}
 	
 // Determinant
-	//Parameters: Matrix and its dimension 
+//Parameters: Matrix and its dimension 
 	public static double getDeterminant(Matrix X) throws Exception {
 		int numRow = X.getNumRows();
     	int numCol = X.getNumCols();
@@ -230,19 +240,124 @@ public class Matrix {
     }
 
 	
-// Reduced Row Echelon Form
-	public double[][] solveRREF(double[][] matrix) {
-		
-		// get reduced row echelon form
-		double[][] result = matrix;
-		
-		// set the rREF variable
-		return result;
+// Row Reducing Matrix
+// Prints steps to console
+// Returns a new Matrix with the reduced
+    
+	public static Matrix rowReduce(Matrix M) {
+		double[][] m = M.getMatrix();
+		int pivotNum = 0; //index where currently examining
+	    int row = m.length;
+	    int col = m[0].length;	    
+	    int rowOn; //row currently examining 	    
+	    boolean end = false;
+	    
+	    for(int r = 0; r < row && !end; r++) {
+	        print(m); //continuously print out the steps to row-reduce 
+	        
+	        System.out.println();
+
+	        if(col <= pivotNum) {//When checking the elements in row, it reached the end
+	            end = true;
+	            break; //terminate loop
+	        }
+
+	        rowOn=r;
+
+	        while(!end && m[rowOn][pivotNum] == 0) { //Checking the numbers going down the column, if that number is 0, continue down
+	        	rowOn++; //Increment rowOn if that element is 0
+	            
+	            if(row == rowOn) { //hit the end of the matrix's column
+	                rowOn=r; 	                
+	                pivotNum++; // Allows the matrix to check the next column
+	                if(col == pivotNum) { //We know that max number of pivots is max number of row. m[0].length is really how many rows there are when you think about it.This basically tells us when we hit the end of the matrix. 
+	                    end = true;
+	                    break;
+	                }
+	            }
+	        }
+	       
+	        if(!end && m[rowOn][pivotNum] != 0) {//goes here if that index from above isn't 0
+	        	swapRows(m, rowOn, r); //First thing it does is try to swap. Won't affect matrix if it can't swap anyway (aka "swap with itself")
+	        	
+	            if(m[r][pivotNum] != 0) {//As long as that element isn't 0 ...
+	                multiplyRow(m, r, (1.0 / m[r][pivotNum])); //Multiply CURRENT row by scalar multiple 
+	            }
+
+	            for(rowOn = 0; rowOn < row; rowOn++) { //this for loop makes everything else (except pivot) 0 in that column 
+	                if(rowOn != r) { //if it's not the same row, then add CURRENT row to the other rows
+	                    addRows(m, m[rowOn][pivotNum], r, rowOn);
+	                }
+	            }
+	        }
+	    }
+	    return new Matrix(m);
 	}
 	
-	public double[][] getRREF() {
-		return this.rREF;
+	private static void swapRows(double [][] m, int row1, int row2)
+	{
+		//temporary array for swapping
+	    double [] temp = new double[m[0].length];
+
+	    //filling up swap array with the values in the first row
+	    for(int c = 0; c < m[0].length; c++)
+	    {
+	        temp[c] = m[row1][c];
+	    }
+	    
+	    for(int c2 = 0; c2 < m[0].length; c2++)
+	    {
+	        m[row1][c2] = m[row2][c2];
+	        m[row2][c2] = temp[c2];
+	    }
 	}
+
+	private static void multiplyRow(double [][] m, int row, double scalar)
+	{
+		//multiplying specific row with a scalar
+		//no need for nested for loops 
+	    for(int c1 = 0; c1 < m[0].length; c1++)
+	    {
+	        m[row][c1] = m[row][c1]*scalar;
+	    }
+	}
+
+	private static void addRows(double [][] m, double scalar, int add_scalar_times_this_row, int from_this_row)
+	{
+		//same as above, no need for nested for loops
+		//meant to turn row element into 0 
+	    for(int c1 = 0; c1 < m[0].length; c1++)
+	    {
+	        m[from_this_row][c1] = (m[from_this_row][c1]) - (scalar * m[add_scalar_times_this_row][c1]);
+	    }
+	}
+
+	private static void print(double [][] matrix)
+	{
+		//printing out the matrix like this: [   ,   ,   ]
+		for(int r = 0; r < matrix.length; r++)
+		{
+			System.out.print("[ ");
+			
+			for(int c = 0; c< matrix[0].length; c++)
+			{
+				if(c == matrix[0].length -1 )
+				{
+					System.out.print(matrix[r][c]);
+				}
+				else
+				{
+					System.out.print(matrix[r][c] + ", ");
+				}
+			}
+			
+			System.out.println(" ]");
+		}
+		
+	}
+
+// End of Row Reduce 
+	
 
 // Transpose of a matrix
 	public static double[][] transpose(double[][] m) {
@@ -258,6 +373,7 @@ public class Matrix {
 		return t;
 	}
 
+// End of Transpose
 	
 // toString method - useful for command line based testing
 	
@@ -282,9 +398,4 @@ public class Matrix {
 		
 		return str.toString();
 	}
-	
-
-	
-	
-	
 }
